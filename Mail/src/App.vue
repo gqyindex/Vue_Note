4be406f5f08c0add11493e723a0e23146ea4a9ac
gqyindex <1391533358@qq.com>
2019-03-26 09:09:27 +0800

    sort by price

---
+++ Mail/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import GoodList from "./views/GoodList";
+export default {
+  name: 'App',
+  components: {GoodList},
+}
+</script>
+
+<style>
+
+</style>
